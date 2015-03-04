a06513d4f6e35c6a4d62ea38d66626eecaf44a74
Austin Garcia <austing8012@gmail.com>
2015-03-04 13:54:06 -0700

    display justins interface in laravel

--- rockitdogs/resources/views/home.blade.php
+++ rockitdogs/resources/views/home.blade.php
@@ -1,17 +1,52 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>RockIt Dogs</title>
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav>
+		<div class="nav-logo">
+			<span><img src="logo.png"</span>
+			<span>RockItDogs</span>
+		</div>
+		<ul>
+			<li>Home</li>
+			<li>Browse</li>
+			<li>About</li>
+			<li>ContactUs</li>
+			<li>LogIn</li>
+			<li>Register</li>
+		</ul>
+	</nav>
+	<main>
+		<h1>Meet rockIT Dogs</h1>
+		<p>love your life, love your dogs<p>	
+	</main>
+	<div class="submain">
+		<h1>Give Love EryDay</h1>
+		<button>Love</button>
+		<div>
+			<img src="logo.png">
 		</div>
 	</div>
-</div>
+	<div class="bottom">
+		<button>SignUp</button> or
+		<button>LogIN</button>
+	</div>
+	<footer>
+		<ul>
+			<li>Home *</li>
+			<li>Browse *</li>
+			<li>About *</li>
+			<li>ContactUs</li>
+		</ul>
+	</footer>
+</body>
+</html>
 @endsection
