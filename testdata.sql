c7c20ba93998f21494d2f40210bb270e76cb1c47
Jessica Collins <jessicodes@gmail.com>
2015-03-04 17:11:45 -0700

    Moving .env file back to root for Thomas

---
+++ testdata.sql
@@ -0,0 +1,6 @@
+
+/*Add Test Addresses */
+INSERT INTO `address` (`address_id`, `address1`, `address2`, `city`, `state`, `zip`) VALUES (NULL, '2343 South Port St.', NULL, 'Scottsdale', 'AZ', '85308');
+INSERT INTO `address` (`address_id`, `address1`, `address2`, `city`, `state`, `zip`) VALUES (NULL, '4562 W. Shore Ln.', NULL, 'Tempe', 'AZ', '85932');
+
+
