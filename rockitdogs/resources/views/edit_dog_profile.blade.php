66cb40d0ce33f0b1606a6ced4ea91960dd943547
Thomas <tlontz1@gmail.com>
2015-03-06 14:53:09 -0700

    added a header to editdogprofile.html and added the view for the page

---
+++ rockitdogs/resources/views/edit_dog_profile.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('header')
+@show
+
+@section('main_content')
+		<h1>Here you can edit your dog's profile</h1>
+		<form action="/dogprofile">
+			Dog Name:<input type="text">
+			Tagline:<input type="text">
+			Birthday:<input type="text">
+			Breed:<input type="text">
+			Gender:<input type="text">
+			Weight:<input type="text">
+			Tempermant:<input type="text">
+			License ID:<input type="text">
+			<button>Submit</button>
+		</form>
+	</main>
+@show
+
+@section('footer')
+@show
