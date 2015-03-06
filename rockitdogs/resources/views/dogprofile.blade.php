6c27006ea7fe72e645dae89cd05778e20c8f96cb
jell1 <jell_1@yahoo.com>
2015-03-06 12:04:23 -0700

    refit dogprofile for our layout view

--- rockitdogs/resources/views/dogprofile.blade.php
+++ rockitdogs/resources/views/dogprofile.blade.php
@@ -4,39 +4,28 @@
 
 @endsection
 
-@section('content')
-	<main>
-		<div class="header">
-			<a href="/editdogprofile">Edit</a>
-			<h1>Welcome To Your Dog's Profile Page</h1>
-			<p>This page contains all the info about your dog as well as all of it's adorable photos!<p>
-		</div>
-		<div class="profile">
-			<div class="dogprofilepicture">
-				<img src="" alt="">
-			</div>
+@section('main_content')
+	<div class="header">
+		<a href="/editdogprofile">Edit</a>
+		<h1>Welcome To Your Dog's Profile Page</h1>
+		<p>This page contains all the info about your dog as well as all of it's adorable photos!<p>
+	</div>
+	<div class="profile">
+		<div class="dogprofilepicture">
 			<img src="" alt="">
-			<h3>Here is all of your dog's info</h3>
-			<div>Sir Charles</div>
-			<div>"I am the classiest dog you will ever meet"</div>
-			<div>2012/03/02</div>
-			<div>Pug</div>
-			<div>Male</div>
-			<div>20 llbs</div>
-			<div>Swagtastic</div>
-			<div>827689</div>
 		</div>
-	</main>
-	<div class="submain">
-		<h1>Give Love EryDay</h1>
-		<button>Love</button>
-		<div>
-			<img src="logo.png">
-		</div>
+		<img src="" alt="">
+		<h3>Here is all of your dog's info</h3>
+		<div>Sir Charles</div>
+		<div>"I am the classiest dog you will ever meet"</div>
+		<div>2012/03/02</div>
+		<div>Pug</div>
+		<div>Male</div>
+		<div>20 llbs</div>
+		<div>Swagtastic</div>
+		<div>827689</div>
 	</div>
 @endsection
-
-
 
 @section('footer')
 @endsection
