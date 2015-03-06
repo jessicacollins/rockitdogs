1d63d493104170a723d7c2ed2d2c962c1667beb5
jell1 <jell_1@yahoo.com>
2015-03-06 11:39:49 -0700

    added registration view

--- rockitdogs/app/Http/routes.php
+++ rockitdogs/app/Http/routes.php
@@ -18,6 +18,10 @@ Route::get('/','WelcomeController@index');
 
 Route::get('home','HomeController@index');
 
+Route::get('test', function () {
+	return view('homepage');
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
