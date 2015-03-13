f7236ec5aaad9cd959776f1adfa81ed49a4a6b40
jell1 <jell_1@yahoo.com>
2015-03-13 15:29:25 -0700

    added links on community page

--- rockitdogs/resources/views/community.blade.php
+++ rockitdogs/resources/views/community.blade.php
@@ -3,13 +3,14 @@
 @section('main_content')
 	<div class="community-grid">
 @foreach($dog as $d)
+		<a href="/dogprofile/{{$d->image_id}}">
+			<div class="dog-image" style="background-image: url({{$d->url}})" data-image-id="{{$d->image_id}}">
+				<div class="dog-name">{{$d->name}}</div>
+				<div class="love">{{$d->getImageLoveCount($d->image_id)}}</div>
+			
+			</div>
+		</a>
 
-		<div class="dog-image" style="background-image: url({{$d->url}})" data-image-id="{{$d->image_id}}">
-			<div>{{$d->name}}</div>
-			<div class="love">{{$d->getImageLoveCount($d->image_id)}}</div>
-		
-		</div>
-		
 @endforeach
 	</div>
 @stop
